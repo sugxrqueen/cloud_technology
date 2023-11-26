a9dff410d2c3eb47ce70055cd444bda0dc3632ee
sugxrqueen <143545231+sugxrqueen@users.noreply.github.com>
2023-11-25 21:33:12 -0500

    Delete project 2 mccormck directory

--- project 2 mccormck/cart.js
+++
@@ -1,6 +0,0 @@
-if($_SERVER['PHP_SELF']!="CART FILE"){             
-    echo "<h1>Cart Contents</h1>";             
-    echo "<div class=\"p2\">";             
-
-    echo "<a href=\"cart.php\">You have ".count($_SESSION['cart'])." total items in your cart.</a>";
-}
