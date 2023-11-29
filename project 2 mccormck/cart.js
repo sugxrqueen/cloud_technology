a418757dc0ae0013cb1b7fa0af75de74ce26ea96
sugxrqueen <143545231+sugxrqueen@users.noreply.github.com>
2023-11-28 21:18:38 -0500

    Add files via upload

---
+++ project 2 mccormck/cart.js
@@ -0,0 +1,21 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+}
+
+
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+
